fd76e6316611f7db29d32fcab66df81922c62bb4
yoshi-isle <123754082+yoshi-isle@users.noreply.github.com>
2024-08-08 15:23:44 -0400

    Generate json file when creating a tree

--- binary-trees/Program.cs
+++ binary-trees/Program.cs
@@ -1,4 +1,12 @@
 ﻿using BinaryTrees.Models;
 using BinaryTrees.Generators;
+using System.Text.Json;
 
 Node test = BinaryTreeGenerator.GenerateBinaryTree(100);
+JsonSerializerOptions options = new()
+{
+    MaxDepth = 1000 //TODO - Temp solution to solve the nesting
+};
+
+var json = JsonSerializer.Serialize(test, options);
+File.WriteAllText($"generated-trees/binarytree-{Guid.NewGuid()}.json", json);
